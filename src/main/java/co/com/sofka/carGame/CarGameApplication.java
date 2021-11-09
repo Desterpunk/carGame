91c4f5c912ac0652997828a62138c9a1302152f6
Jhon Serna <jhonalfaber@gmail.com>
2021-11-09 11:02:02 -0500

    Inicio proyecto esqueleto base

---
+++ src/main/java/co/com/sofka/carGame/CarGameApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.carGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/carGame/CarGameApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.carGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
